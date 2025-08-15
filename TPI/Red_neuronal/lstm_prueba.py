6cbe027ac6ce33c442b1471fac0ff81be3a03e4b
Juan Cruz Mondino <juancm.2000@hotmail.com>
2025-08-15 14:29:48 -0300

    Empiezo con la combinación de archivos para obtener el necesario para entrenar el LSTM

--- TPI/Red_neuronal/lstm_prueba.py
+++ TPI/Red_neuronal/lstm_prueba.py
@@ -32,6 +32,7 @@ latitud_ejemplo = -31.4
 longitud_ejemplo = -64.2
 años_atras_ejemplo = 44
 
+# Preparo los archivos necesarios para entrenar el modelo
 df_clima_diario = obtener_datos_nasa_power(latitud_ejemplo, longitud_ejemplo, años_atras_ejemplo)
 if df_clima_diario.empty: #Si no hay datos climáticos para las coordenadas y años especificados terminar la ejecución
     raise ValueError("No se encontraron datos climáticos para las coordenadas y años especificados.")
@@ -47,9 +48,39 @@ df_semillas = df_semillas[df_semillas['fecha'].dt.year >= min_year + 1] # Asegur
 
 df_suelo = pd.read_csv('Recuperacion_de_datos/Suelos/suelo_unido.csv')
 
+# Debo limpiar las columnas con muchos datos faltantes porque interfieren mucho en el entreno de la RN.
+# Elimino las columnas que tienen 85% o más datos faltantes
+df_suelo_filtrado = df_suelo.dropna(thresh=len(df_suelo) * 0.15, axis=1)
+df_clima_filtrado = df_clima.dropna(thresh=len(df_clima) * 0.15, axis=1)
+df_semillas_filtrado = df_semillas.dropna(thresh=len(df_semillas) * 0.15, axis=1)
 
+
+# Crear nueva columna 'departamento' indicando a que departamento corresponde cada dato de suelo
+df_suelo_filtrado['departamento_nombre'] = df_suelo_filtrado.apply(
+    lambda row: transformo_coord_a_depto([row['latitude'], row['longitude']]),
+    axis=1
+)
+# Eliminar columnas latitude y longitude
+df_suelo_filtrado = df_suelo_filtrado.drop(columns=['latitude', 'longitude'])
+# Reordenar para que 'departamento' quede como primera columna
+cols = ['departamento_nombre'] + [col for col in df_suelo_filtrado.columns if col != 'departamento_nombre']
+df_suelo_filtrado = df_suelo_filtrado[cols]
+
+# Calcular el promedio de las columnas numéricas para cada departamento
+df_suelo_promedio = df_suelo_filtrado.groupby('departamento_nombre', as_index=False).mean(numeric_only=True)
+
+
+# ---------------------------------------------
+# UNIÓN DE LOS DATOS EN UN SOLO DATAFRAME
 # Crear archivo con todos los datos necesarios, las columnas mostraran los datos de las semillas, una columna que tenga una matriz con los datos climásticos de los últimos
 # n meses, o una columna por cada mes previo y en esa columna tener un array de los datos climáticos (o viceversa), y las columnas de los datos del suelo.
+# ---------------------------------------------
+
+# Combino los datos de las semillas con los datos del suelo por medio del departamento
+df_combino_semillas_suelo = pd.merge(df_semillas_filtrado, df_suelo_promedio, on='departamento_nombre', how='inner')
+
+# Ordeno por columna "anio"
+df_combino_semillas_suelo = df_combino_semillas_suelo.sort_values(by='anio').reset_index(drop=True)
 
 
-# Debo limpiar las columnas con muchos datos faltantes porque interfieren mucho en el entreno de la RN.
+
